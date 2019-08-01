4716b9dc542f90e28decd8ae5589e566c6b0df3c
gotylergo <tk03759@gmail.com>
2019-07-31 21:45:26 -0700

    Add time

--- js/index.js
+++ js/index.js
@@ -1,30 +1,46 @@
 let gallons = 0;
 let gpm = 2.5;
+let time = 0;
+let minutes = 0;
+let seconds = 0;
 
-let setGallons = function() {
-  gallons = gallons + (gpm/60);
-  document.getElementById('timer').innerHTML = gallons.toFixed(2);
+let setGallons = function () {
+  gallons = gallons + (gpm / 60);
+  document.getElementById('gallonTimer').innerHTML = gallons.toFixed(2);
+}
+let setTimer = function () {
+  time = time + 1;
+  minutes = Math.floor(time / 60);
+  seconds = time % 60;
+  if (seconds < 10) {
+    seconds = "0" + seconds.toString();
+  } 
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
 }
 
-let myTimer = function() {};
+let myGallonTimer = function () { };
+let myTimer = function () { };
 
-function startTimer () {
+function startTimer() {
   document.getElementById('startScreen').style.display = "none";
   document.getElementById('timerScreen').style.display = "block";
 
   gpm = document.getElementById('gpm').value;
 
-  myTimer = setInterval(setGallons, 1000);
+  myGallonTimer = setInterval(setGallons, 1000);
+  myTimer = setInterval(setTimer, 1000)
 
 }
 
-function stopTimer () {
+function stopTimer() {
+  clearInterval(myGallonTimer);
   clearInterval(myTimer);
   document.getElementById('stopButton').style.display = "none";
   document.getElementById('resetButton').style.display = "block";
 }
 
-function resetTimer () {
+function resetTimer() {
   document.getElementById('timerScreen').style.display = "none";
   document.getElementById('startScreen').style.display = "block";
   document.getElementById('stopButton').style.display = "block";
